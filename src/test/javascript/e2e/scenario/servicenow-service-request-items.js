ce7e19a93e4e029203efa7d7b89e21eea64f3b03
saraswat2385 <saraswat2385@gmail.com>
2018-07-30 16:40:42 +0530

    wip

--- src/test/javascript/e2e/scenario/servicenow-service-request-items.js
+++ src/test/javascript/e2e/scenario/servicenow-service-request-items.js
@@ -1,61 +1,78 @@
-// /*
-//  * Copyright (c) 2018. All rights reserved.
-//  *
-//  * This software and all trademarks, trade names, and logos included herein are the property of XebiaLabs, Inc. and its affiliates, subsidiaries, and licensors.
-//  */
-// 'use strict'
-//
-// const serviceNowServer = 'ServiceNowServer';
-//
-// const openshiftClientConfig = 'Openshift Client Configuration';
-//
-// const timeout = 180000;
-//
-//
-// describe('servicenow create and access service', () => {
-//     beforeAll(()=>{
-//         LoginPage.login('admin', 'admin');
-//     });
-//
-//     afterAll(()=>{
-//             LoginPage.logout();
-//         });
-//     beforeEach(() => {
-//     fixtures().release({
-//                 id: 'ReleaseWithServiceNowServiceRequestItem',
-//                 title: 'ReleaseWithServiceNowServiceRequestItem',
-//                 description: 'ReleaseWithServiceNowServiceRequestItem',
-//                 status: 'planned',
-//                 phases: [
-//                     {
-//                         title: 'Phase',
-//                         status: 'PLANNED',
-//                         tasks: []
-//                     }
-//                 ]
-//             });
-//     Page.openConfiguration()
-//                 .addNewInstance('ServiceNow: Server')
-//                 .setTextField('url', browser.params.servicenow.address)
-//                 .setTextField('username', browser.params.servicenow.username)
-//                 .setTextField('password', browser.params.servicenow.password)
-//                 .save();
-//
-//     afterEach(() => {
-//             fixtures().clean();
-//             Page.openConfiguration().deleteInstance(openshiftClientConfig);
-//             Page.openConfiguration().deleteInstance(serviceNowServer);
-//         });
-//
-//     it('should create Servicerequest Items', () => {
-//             utils.addFirstTask(addTaskTitle, 'openshift.createConf')
-//                  .configureTask(addTaskTitle, jsonTaskConfig)
-//                  .addAnotherTask(startCheckServiceTaskTitle, 'openshift.checkService')
-//                  .configureTask(startCheckServiceTaskTitle, checkServiceTaskConfig)
-//                  .addAnotherTask(deleteTaskTitle, 'openshift.removeConf')
-//                  .configureTask(deleteTaskTitle, jsonTaskConfig)
-//                  .startReleaseAndWait(timeout);
-//              utils.assertOutput(startCheckServiceTaskTitle, 'Service hello-service is accessible')
-//         });
-// });
-// });
+describe('Service Item', function () {
+    globalForEach();
+
+    beforeEach(function () {
+        fixtures().ci({
+            id: 'Configuration/Custom/ConfigurationServiceNow',
+            title: 'Service Now Server',
+            type: 'servicenowxl.Server',
+            url: browser.params.servicenow.address,
+            username: browser.params.servicenow.username,
+            password: browser.params.servicenow.password
+        });
+
+        fixtures().release({
+            id: 'ReleaseCreateServiceItem',
+            title: 'Create Service Item',
+            status: 'planned',
+            scheduledStartDate: moment().subtract(3, 'days'),
+            dueDate: moment().add(8, 'days'),
+            phases: [{
+                title: 'Prod',
+                status: 'planned',
+                tasks: [{
+                    title: 'Create Service item ',
+                    type: 'xlrelease.CustomScriptTask',
+                    status: 'planned',
+                    owner: 'admin',
+                    pythonScript: {
+                        type: 'servicenowxl.CreateRequestItem',
+                        servicenowServer: 'Configuration/Custom/ConfigurationServiceNow',
+                        shortDescription : 'description',
+                        comments: 'comments'
+                    }
+                }, {
+                    title: 'Create Service item ',
+                    type: 'xlrelease.CustomScriptTask',
+                    status: 'planned',
+                    owner: 'admin',
+                    pythonScript: {
+                        type: 'servicenowxl.CreateNewRequestItem',
+                        servicenowServer: 'Configuration/Custom/ConfigurationServiceNow',
+                        content : '{"short_description":"New","comments":"New"}'
+                    }
+                }, {
+                    title: 'Create Service item ',
+                    type: 'xlrelease.CustomScriptTask',
+                    status: 'planned',
+                    owner: 'admin',
+                    pythonScript: {
+                        type: 'servicenowxl.UpdateRequestItem',
+                        servicenowServer: 'Configuration/Custom/ConfigurationServiceNow',
+                        content : '{"short_description":"Updated","comments":"Updated"}',
+                        sysId: ''
+                    }
+                }, {
+                    title: 'Create Service item ',
+                    type: 'xlrelease.CustomScriptTask',
+                    status: 'planned',
+                    owner: 'admin',
+                    pythonScript: {
+                        type: 'servicenowxl.FindRequestItemByTicket',
+                        servicenowServer: 'Configuration/Custom/ConfigurationServiceNow',
+                        ticket : ''
+                    }
+                }]
+            }]
+        });
+        return LoginPage.login('admin', 'admin');
+    });
+
+    it('Run release', function () {
+        let release = Page.openRelease('ReleaseCreateServiceItem');
+
+        return release.start().waitForCompletion();
+
+    });
+
+});
