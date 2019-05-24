08b12d1f4be0384708962fe4f0841169ca28fff6
Mario Sarcevic <mario.sarcevic@ag04.com>
2019-05-24 11:40:07 +0200

    REL-8300 Update ItsmFacet properties for hyperlink support

--- src/main/resources/servicenow/__init__.py
+++ src/main/resources/servicenow/__init__.py
@@ -17,12 +17,12 @@ def add_code_compliance_facet(table_name, facet_api, task, service_now_server=No
             facet.targetId = task.id
             facet.serverUrl = service_now_server['url'] if service_now_server else None
             facet.serverUser = service_now_user or (service_now_server['username'] if service_now_server else None)
-            facet.recordNumber = data['number'] if data and 'number' in data else None
+            facet.record = data['number'] if data and 'number' in data else None
             facet.title = data['short_description'] if data and 'short_description' in data else None
             facet.status = data['state'] if data and 'state' in data else None
             facet.priority = data['priority'] if data and 'priority' in data else None
             facet.createdBy = data['sys_created_by'] if data and 'sys_created_by' in data else None
-            facet.recordUrl = url
+            facet.record_url = url
             facet_api.createFacet(facet)
         except:
             exctype, value = sys.exc_info()[:2]
